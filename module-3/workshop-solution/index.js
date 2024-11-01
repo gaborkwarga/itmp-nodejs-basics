b33b1328a15b466ab9653e8bc0cd4494591569d0
Olivér Mrakovics <oliver.mrakovics@gmail.com>
2024-11-01 15:33:01 +0100

    feat: add module 3 solution

--- module-3/workshop-solution/index.js
+++ module-3/workshop-solution/index.js
@@ -1,57 +1,105 @@
-const { PrismaClient } = require("@prisma/client");
 const express = require("express");
 const app = express();
 
-const prisma = new PrismaClient({ log: ["query"] });
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("./database.sqlite");
+
+let users = [
+  {
+    id: "1",
+    name: "John Doe",
+    email: "john.doe@example.com",
+  },
+  {
+    id: "2",
+    name: "Jane Smith",
+    email: "jane.smith@example.com",
+  },
+  {
+    id: "3",
+    name: "Sam Johnson",
+    email: "sam.johnson@example.com",
+  },
+];
+
+db.serialize(() => {
+  db.run("DROP TABLE IF EXISTS users"); // never do this in production
+  db.run(
+    "CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT)"
+  );
+
+  for (const i of users) {
+    db.run("INSERT INTO users (name, email) VALUES (?, ?)", [i.name, i.email]);
+  }
+});
 
 app.use(express.json());
 
-app.get("/api/users", async (req, res) => {
-  const users = await prisma.user.findMany();
-  res.json(users);
-});
+app.get("/api/users", (req, res) => {
+  db.all("SELECT * FROM users", (err, users) => {
+    if (err) {
+      return res.status(500).json({ message: err.message });
+    }
 
-app.get("/api/users/:id", async (req, res) => {
-  const id = req.params.id;
-  const user = await prisma.user.findUnique({ where: { id: id } });
-
-  if (!user) {
-    return res.status(404).json({ message: "User not found" });
-  }
-
-  res.json(user);
-});
-
-app.post("/api/users", async (req, res) => {
-  const newUser = await prisma.user.create({ data: req.body });
-  res.status(201).json(newUser);
-});
-
-app.put("/api/users/:id", async (req, res) => {
-  const id = req.params.id;
-  const user = await prisma.user.findUnique({ where: { id: id } });
-
-  if (!user) {
-    return res.status(404).json({ message: "User not found" });
-  }
-
-  const updatedUser = await prisma.user.update({
-    where: { id: id },
-    data: req.body,
+    res.json(users);
   });
-  res.json(updatedUser);
 });
 
-app.delete("/api/users/:id", async (req, res) => {
+app.get("/api/users/:id", (req, res) => {
   const id = req.params.id;
-  const user = await prisma.user.findUnique({ where: { id: id } });
+  db.get("SELECT * FROM users WHERE id = ?", [id], (err, user) => {
+    if (err) {
+      return res.status(500).json({ message: err.message });
+    }
 
-  if (!user) {
-    return res.status(404).json({ message: "User not found" });
-  }
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
 
-  await prisma.user.delete({ where: { id: id } });
-  res.sendStatus(204);
+    res.json(user);
+  });
+});
+
+app.post("/api/users", (req, res) => {
+  db.run(
+    "INSERT INTO users (name, email) VALUES (?, ?)",
+    [req.body.name, req.body.email],
+    function (err) {
+      if (err) {
+        return res.status(500).json({ message: err.message });
+      }
+
+      res.status(201).json({ ...req.body, id: this.lastID });
+    }
+  );
+});
+
+app.put("/api/users/:id", (req, res) => {
+  const id = req.params.id;
+
+  db.run(
+    "UPDATE users SET name = ?, email = ? WHERE id = ?",
+    [req.body.name, req.body.email, id],
+    function (err) {
+      if (err) {
+        return res.status(500).json({ message: err.message });
+      }
+
+      res.json({ ...req.body, id: Number(id) });
+    }
+  );
+});
+
+app.delete("/api/users/:id", (req, res) => {
+  const id = req.params.id;
+
+  db.run("DELETE FROM users WHERE id = ?", [id], function (err) {
+    if (err) {
+      return res.status(500).json({ message: err.message });
+    }
+
+    res.sendStatus(204);
+  });
 });
 
 app.listen(3000, () => console.log("The app is running on port 3000!"));
