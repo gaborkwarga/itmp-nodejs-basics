4d927cb1e7946928fa5b7439f3dcfb7b54869653
Oliver Mrakovics <oliver.mrakovics@gmail.com>
2024-11-08 15:46:37 +0100

    feat(module-1): add index.html and a bonus task

--- module-1/workshop-solution/index.js
+++ module-1/workshop-solution/index.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/itmp", (req, res) => {
+  res.send("Hello ITMP!");
+});
+
+app.get("/nodejs", (req, res) => {
+  res.send(
+    "Fogalmazd meg a saját szavaiddal, hogy mi az a Node.js és mire használható!"
+  );
+});
+
+app.get("/express", (req, res) => {
+  res.send(
+    "Fogalmazd meg a saját szavaiddal, hogy mi az az Express.js és mire használható!"
+  );
 });
 
 app.listen(3000, () => {
